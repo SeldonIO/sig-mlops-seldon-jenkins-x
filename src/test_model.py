85a0ec423af9b2726b37c7c7f84874e31b2a92a7
Alejandro Saucedo <axsauze@gmail.com>
2019-11-07 15:55:20 +0000

    Added requirements

--- src/test_model.py
+++ src/test_model.py
@@ -2,12 +2,13 @@
 import numpy as np
 from unittest import mock
 import joblib
+import os
 
 EXPECTED_RESPONSE = np.array([3, 3])
 
 def test_model(*args, **kwargs):
     data = ["text 1", "text 2"]
 
-    m = joblib.load("model.joblib")
+    m = joblib.load(f"{os.getcwd()}/model.joblib")
     result = m.predict(data)
     assert all(result == EXPECTED_RESPONSE)
