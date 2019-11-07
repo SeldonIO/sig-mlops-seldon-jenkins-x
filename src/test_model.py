6244235fb82683745ea7b37d4924da6a7ea49105
Alejandro Saucedo <axsauze@gmail.com>
2019-11-07 15:58:32 +0000

    added model

--- src/test_model.py
+++ src/test_model.py
@@ -9,6 +9,6 @@ EXPECTED_RESPONSE = np.array([3, 3])
 def test_model(*args, **kwargs):
     data = ["text 1", "text 2"]
 
-    m = joblib.load(f"{os.getcwd()}/model.joblib")
+    m = joblib.load("model.joblib")
     result = m.predict(data)
     assert all(result == EXPECTED_RESPONSE)
